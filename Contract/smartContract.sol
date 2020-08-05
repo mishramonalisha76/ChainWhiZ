2c5f2a1bb7208a2ea97abe5aef65c7c1d9f37854
Rajashree Parhi <rajashreeparhi23@gmail.com>
2020-08-05 22:26:32 +0530

    Add contract

--- Contract/smartContract.sol
+++ Contract/smartContract.sol
@@ -9,26 +9,41 @@ contract IPFS {
    struct quesContractDetails{
          string ipfshash;
          address publisher;
-         uint256 reward;
+         uint256 contractReward;
+         uint256 dappReward;
          uint256 timeStart;
          uint256 timeEnd;
+         uint256 dappTimeEnd;
          string date;
          address resSolver;
          string resSolutionLink;
          string resSolutionLinkReadMe;
-         bool label;
+         
+         string approveDapp;
+         address approveDappSolver;
+         bool contractLabel;
+         bool dappLabel;
+         string typeSol;
+       
+      
         
          }
   
 
-     struct quesIPFS{
+     struct quessmartContract{
             address[] solver;
             string[] solutionLink;
             string[] readMe;
             uint256[] votePercent;
             
         }
-     
+
+     struct quesDapp{
+        address[] dappSolver;
+        string[] dappHash;
+          
+
+      }
 
     struct solLink{
            
@@ -39,44 +54,73 @@ contract IPFS {
 
 
 
+mapping(address=>mapping(string=>quesContractDetails[]))publisherContractProfile;
+
 quesContractDetails[] quesContract;
-mapping(address=> quesContractDetails[]) publisherContractProfile;
+
+
+
 mapping(string =>uint256) contractDetailsIndex;
-mapping(string => quesIPFS) quesSolDetails;
+
+
+mapping(string => quessmartContract) quesSolDetails;
+mapping(string => quesDapp) quesdappSolDetails;
+   
 mapping(string => solLink)solutionList;
 mapping(string => mapping(address => bool)) voted;
+
 mapping(string => mapping(string =>uint256)) solIndex;
+
+mapping(address=>mapping(string=>string))pubQuesIndex;
+mapping(string => mapping(string=>uint256)) index;
 mapping(string => uint256) maxVotedIndex;
+
+
 
 //publisher upload question 
 
- function publisherUploadQues(string memory questionIpfs,uint256 quesReward,string memory dateTime,uint256 start,uint256 end) public{
+ function publisherUploadQues(string memory questionIpfs,uint256 quesReward,uint256 quesDappReward,string memory dateTime,uint256 start,uint256 end,string memory typeOfQues, uint256 dappTime) public{
               
          contractDetailsIndex[questionIpfs]=quesContract.length;
-         
+         pubQuesIndex[msg.sender][typeOfQues]=questionIpfs;
+         index[typeOfQues][questionIpfs]=publisherContractProfile[msg.sender][typeOfQues].length;
+
          quesContract.push(quesContractDetails({
            ipfshash:questionIpfs,
            publisher:msg.sender,
-           reward:quesReward,
+           contractReward:quesReward,
            timeStart:start,
            timeEnd:end,
            date:dateTime,
            resSolver:address(0),
            resSolutionLink:"",
            resSolutionLinkReadMe:"",
-           label:true
+           approveDapp:"",
+           approveDappSolver:address(0),
+          contractLabel:true,
+            dappLabel:false,
+           typeSol:typeOfQues,
+           dappTimeEnd:dappTime,
+           dappReward:quesDappReward
+       
            
 
 
            }));
-         publisherContractProfile[msg.sender].push(quesContract[contractDetailsIndex[questionIpfs]]);
+
+           
+         publisherContractProfile[msg.sender][typeOfQues].push(quesContract[contractDetailsIndex[questionIpfs]]);
           
   
-                
-
+    }
+      
+      
+      //returning publisher's all questions
+      function getAllContract(string memory typeOfQues)public view returns(quesContractDetails[] memory){
           
-
+          return publisherContractProfile[msg.sender][typeOfQues];
       }
+      
 
 //returning question lists for question card
 function questions() public view returns(quesContractDetails[] memory){
@@ -84,6 +128,28 @@ function questions() public view returns(quesContractDetails[] memory){
    return quesContract;
     
     }
+
+
+//push dapp solution
+
+function pushDapp(string memory quesHash,string memory dapp) public
+{
+    
+      quesdappSolDetails[quesHash].dappHash.push(dapp);
+      quesdappSolDetails[quesHash].dappSolver.push(msg.sender);
+        
+    
+}
+
+
+//returning dapp solution lists
+function dappSol(string memory quesHash) public view returns(quesDapp memory){
+
+   return   quesdappSolDetails[quesHash];
+    
+    }
+    
+    
 
 //solver uploading solutions 
 
@@ -95,6 +161,7 @@ function pushSolution(string memory quesHash,string memory sol,string memory rea
   quesSolDetails[quesHash].readMe.push(readMeHash);
   quesSolDetails[quesHash].votePercent.push(0);
   
+
 }
 
 
@@ -165,32 +232,48 @@ function pushSolution(string memory quesHash,string memory sol,string memory rea
 
 //set result
 
-  function setResult(string memory quesHash,address solver,string memory solutionLink,string memory readMe) public 
+  function setContractResult(string memory quesHash,string memory typeOfQues,address solver,string memory solutionLink,string memory readMe,address pub) public 
     {
        
        
        uint256 pos=contractDetailsIndex[quesHash];
-       quesContract[pos].label=false;
+       quesContract[pos].contractLabel=false;
        quesContract[pos].resSolver=solver;
        quesContract[pos].resSolutionLink=solutionLink;
        quesContract[pos].resSolutionLinkReadMe=readMe;
-    //   publisherContractProfile[pub].label=false;
-    //   publisherContractProfile[pub].resSolver=solver;
-    //   publisherContractProfile[pub].resSolutionLink=solutionLink;
-    //   publisherContractProfile[pub].resSolutionLinkReadMe=readMe;
        
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].contractLabel=false;
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].resSolver=solver;
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].resSolutionLink=solutionLink;
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].resSolutionLinkReadMe=readMe;
+
+    if(keccak256(bytes(typeOfQues)) == keccak256(bytes("dapp")))
+            publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].dappLabel=true;
        
         
     }
 
-//publisher profile for smart contract display
+//set result for dapp
 
-function publisherProfile() public view returns(quesContractDetails[] memory){
-return publisherContractProfile[msg.sender];
+function setContractDapp(string memory quesHash,string memory dapp,address solver,string memory typeOfQues,address pub,string memory solutionLink) public
+{
+       uint256 pos=contractDetailsIndex[quesHash];
+       quesContract[pos].dappLabel=false;
+       quesContract[pos].approveDappSolver=solver;
+       quesContract[pos].approveDapp=solutionLink;
+       
+     
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].approveDappSolver=solver;
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].approveDapp=dapp;
+     publisherContractProfile[pub][typeOfQues][index[typeOfQues][pubQuesIndex[pub][typeOfQues]]].dappLabel=false;
+
 
 }
+
+
+
     
-function publisherContractSol(string memory quesHash) public view returns(quesIPFS memory){
+function publisherContractSol(string memory quesHash) public view returns(quessmartContract memory){
 return quesSolDetails[quesHash];
 
 }
